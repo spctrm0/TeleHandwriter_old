8632437757321cd8588a67bf01658197a76211de
spctrm0 <spctrm0@gmail.com>
2017-05-11 11:53:37 +0900

    Beginning of the PenHead dev

---
+++ PenHead/src/tabletInput/Main.java
@@ -0,0 +1,38 @@
+package tabletInput;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet
+{
+	TabletInput		tabletInput;
+
+	public void settings()
+	{
+		size(1280, 720);
+	}
+
+	public void setup()
+	{
+		tabletInput = new TabletInput(this);
+		background(255);
+	}
+
+	public void draw()
+	{
+
+	}
+
+	public void keyPressed()
+	{
+		
+	}
+
+	static public void main(String[] passedArgs)
+	{
+		String[] appletArgs = new String[] { tabletInput.Main.class.getName() };
+		if (passedArgs != null)
+			PApplet.main(concat(appletArgs, passedArgs));
+		else
+			PApplet.main(appletArgs);
+	}
+}
